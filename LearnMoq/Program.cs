9a03de97a025d42c4f9929560e226990c49585c6
Samson Amaugo <sammychinedu2ky@gmail.com>
2022-05-05 13:01:13 +0100

    Add project files.

---
+++ LearnMoq/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+var httpClient = new HttpClient {BaseAddress = new Uri("https://jsonplaceholder.typicode.com/todos/")};
+var logger = new TodoCustomLogger(httpClient, 1);
+await logger.CustomLogger();
+
+public class Todo
+{
+    [JsonPropertyName("id")] public int Id { get; set; }
+
+    [JsonPropertyName("userId")] public int UserId { get; set; }
+
+    [JsonPropertyName("title")] public string Title { get; set; } = "";
+
+    [JsonPropertyName("completed")] public bool Completed { get; set; }
+}
+
+public class TodoCustomLogger
+{
+    private readonly HttpClient _client;
+
+    public TodoCustomLogger(HttpClient client, int id)
+    {
+        _client = client;
+        Id = id;
+    }
+
+    private int Id { get; }
+
+    public async Task<string> CustomLogger()
+    {
+        var response = await _client.GetAsync(Convert.ToString(Id));
+        var responseBody = await response.Content.ReadAsStringAsync();
+        var deserialized = JsonSerializer.Deserialize<Todo>(responseBody)!;
+        return deserialized.Completed
+            ? $"Your todo: {deserialized.Title} has been completed "
+            : $"Your todo: {deserialized.Title} is yet to be completed ";
+    }
+}
+
+
+public class CustomHandler : HttpClientHandler
+{
+    protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request,
+        CancellationToken cancellationToken)
+    {
+        return Task.FromResult(new HttpResponseMessage
+        {
+            Content = new StringContent(
+                "{ 'userId': 1,  'id': 38,  'title': 'fugiat veniam minus',   'completed': false }")
+        });
+    }
+}
---
+++ LearnMoqTests/TodoCustomLoggerTests.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Moq.Protected;
+
+//using Program;
+
+namespace Tests;
+
+[TestClass]
+public class TodoCustomLoggerTests
+{
+    private readonly string baseUri = "https://jsonplaceholder.typicode.com/todos/";
+
+
+    [TestMethod]
+    public void LogTodoIsCompleted()
+    {
+        //arrange
+        var todo = new Todo
+        {
+            UserId = 1,
+            Id = 38,
+            Title = "code",
+            Completed = true
+        };
+        var responseMessage = new HttpResponseMessage
+        {
+            Content = new StringContent(JsonSerializer.Serialize(todo))
+        };
+        var moq = new Mock<HttpClientHandler>();
+        moq.Protected().Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(),
+            ItExpr.IsAny<CancellationToken>()).ReturnsAsync(responseMessage);
+        var handler = moq.Object;
+        var client = new HttpClient(handler);
+        client.BaseAddress = new Uri(baseUri);
+        //act
+        var logger = new TodoCustomLogger(client, 1);
+
+
+        //assert
+        var expected = $"Your todo: {todo.Title} has been completed ";
+        Assert.AreEqual(expected, logger.CustomLogger().Result);
+    }
+
+    [TestMethod]
+    public void LogTodoIsNotCompleted()
+    {
+        //arrange
+        var todo = new Todo
+        {
+            UserId = 1,
+            Id = 38,
+            Title = "code",
+            Completed = false
+        };
+        var responseMessage = new HttpResponseMessage
+        {
+            Content = new StringContent(JsonSerializer.Serialize(todo))
+        };
+        var moq = new Mock<HttpClientHandler>();
+        moq.Protected().Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(),
+            ItExpr.IsAny<CancellationToken>()).ReturnsAsync(responseMessage);
+        var handler = moq.Object;
+        var client = new HttpClient(handler);
+        client.BaseAddress = new Uri(baseUri);
+        //act
+        var logger = new TodoCustomLogger(client, 1);
+
+
+        //assert
+        var expected = $"Your todo: {todo.Title} is yet to be completed ";
+        Assert.AreEqual(expected, logger.CustomLogger().Result);
+    }
+}
